fde5ba31a2d61ae591aabc22f731b37dd9eba8f2
sevenample <90945287+sevenample@users.noreply.github.com>
2024-05-03 09:56:50 +0800

    Update 計算機韌體實驗17.cpp

--- 計算機韌體實驗17.cpp
+++ 計算機韌體實驗17.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	//cin.sync_with_stdio(false); cin.tie(nullptr);
-	int number, buffer = 0, counts[32] = {};
-	while (scanf("%d", &number) == 1)
-		for (int i = 0, j = 1; j <= number && i < 32; i++, j <<= 1)
-			if (number & j)
-				counts[i]++;
-	for (int i = 0, j = 1; i < 32; ++i, j <<= 1)
-		if (counts[i] % 3)
-			buffer |= j;
-	printf("%d\n", buffer);
+int main (){
+	int n ; 
+	int arr[10] = {0};
+	while (cin >>n ){
+		arr[n]	++;
+	}
+
+	for (int i = 0; i<10; i++){
+		if (arr[i]%3){
+			cout << i;
+			break;
+		}
+	}
+	
 }
