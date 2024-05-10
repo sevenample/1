edeab63b8ff6fab74f423ba4d9eadb94427b1b24
sevenample <90945287+sevenample@users.noreply.github.com>
2024-05-10 10:33:14 +0800

    Update 計算機韌體實驗20.cpp

--- 計算機韌體實驗20.cpp
+++ 計算機韌體實驗20.cpp
@@ -5,26 +5,24 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	int n;
+	int n,d;
 	while (cin >> n && n) {
-
-		priority_queue<int, vector<int>, greater<int>> line;
-		int input, temp, result = 0;
-
-		while (n--) cin >> input, line.emplace(input);
-		
-		while (line.size() > 1) {
-			temp = line.top(), line.pop();
-			temp += line.top(), line.pop();
-			line.emplace(temp);
-			result += temp;
+		priority_queue<int, vector<int>, greater<int>> pq;
+		for (int i = 0;i<n;i++)
+			{
+				cin >>d;
+				pq.push(d);
+			}
+		long long sum ,cost =0;
+		while (pq.size() !=1)	{
+			sum = pq.top();
+			pq.pop();
+			sum+=pq.top();
+			pq.pop();
+			cost +=sum;
+			pq.push(sum);
 		}
-
-		cout << result << "\n";
+		cout << cost <<endl;
 	}
 
 	return 0;
